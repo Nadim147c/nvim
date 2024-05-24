de2333173fa03270f6fdd5f43e9433d43ffdb70b
Ephemeral <90786729+Nadim147c@users.noreply.github.com>
2024-05-24 15:08:05 +0000

    add condition for buffer name and lsp on lualine

--- lua/configs/lualine.lua
+++ lua/configs/lualine.lua
@@ -67,13 +67,26 @@ insert_left {
     diagnostics_color = {},
 }
 
+local function lsp_condition()
+    local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+    local clients = vim.lsp.get_clients()
+    if next(clients) == nil then
+        return false
+    end
+    for _, client in ipairs(clients) do
+        local filetypes = client.config.filetypes
+        if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+            return true
+        end
+    end
+    return false
+end
 insert_right {
     function()
-        local msg = "No Active Lsp"
         local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-        local clients = vim.lsp.get_active_clients()
+        local clients = vim.lsp.get_clients()
         if next(clients) == nil then
-            return msg
+            return
         end
         for _, client in ipairs(clients) do
             local filetypes = client.config.filetypes
@@ -81,14 +94,21 @@ insert_right {
                 return client.name
             end
         end
-        return msg
     end,
     icon = " LSP :",
+    cond = lsp_condition,
     color = { fg = colors.green, gui = "bold" },
 }
 
+local function filename_condition()
+    local current_buf = vim.api.nvim_get_current_buf()
+    local buf_name = vim.api.nvim_buf_get_name(current_buf)
+    return not string.match(buf_name, "NvimTree_.*$")
+end
+
 insert_right {
     "filename",
+    cond = filename_condition,
     color = { fg = colors.teal, gui = "bold" },
 }
 
