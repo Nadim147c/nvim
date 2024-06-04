2a71f11a434a9e306b67ad09cd7bc1ecf9a1e1e2
Ephemeral <90786729+Nadim147c@users.noreply.github.com>
2024-06-04 09:12:01 +0000

    bruv

--- lua/custom/vim_enter.lua
+++ lua/custom/vim_enter.lua
@@ -1,9 +1,9 @@
 vim.api.nvim_create_autocmd({ "VimEnter" }, {
     group = vim.api.nvim_create_augroup("vim_enter_telescope_or_nvim_tree", { clear = true }),
-    desc = "Highlight selection on yank",
+    desc = "Open telescope or nvim tree one vim enter",
     callback = function(data)
         if vim.fn.argv(0) == "" then
-            require("telescope.builtin").find_files()
+            require("telescope.builtin").git_files()
         end
 
         local directory = vim.fn.isdirectory(data.file) == 1
