5c674fbc4593d37104f356680fe221c6155ba489
Ephemeral <theephemeral.txt@gmail.com>
2025-02-09 13:38:24 +0600

    add column for git commit message filetype
    There are standard length for git commit message. For example the git
    commit message's subject expected to be less then or equal 50 characters
    where descriptions are expected to be less then or equal to 72
    characters if some thing goes more than that then we are doing some
    wrong.

---
+++ after/ftplugin/gitcommit.lua
@@ -0,0 +1,3 @@
+vim.opt_local.textwidth = 72 -- Auto-wrap at 72 characters
+vim.opt_local.colorcolumn = "50,72" -- Visual column guides
+vim.opt_local.spell = true -- Enable spell check
---
+++ lua/plugins/python.lua
@@ -0,0 +1,15 @@
+return {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = {
+        "neovim/nvim-lspconfig",
+        -- "mfussenegger/nvim-dap",
+        -- "mfussenegger/nvim-dap-python", --optional
+        "nvim-telescope/telescope.nvim",
+    },
+    lazy = false,
+    branch = "regexp", -- This is the regexp branch, use this for the new version
+    config = function() require("venv-selector").setup() end,
+    keys = {
+        { ",v", "<cmd>VenvSelect<cr>" },
+    },
+}
