aad9f01b0f25b14826679672ca084d139fe6c008
Ephemeral <theephemeral.txt@gmail.com>
2025-10-29 13:43:10 +0600

    lsp(gopls): update gopls config

--- lsp/gopls.lua
+++ lsp/gopls.lua
@@ -1,11 +1,29 @@
 return {
   settings = {
     gopls = {
-      completeUnimported = true,
-      usePlaceholders = false,
       analyses = {
+        -- fieldalignment = true,
+        -- nilness = true,
         unusedparams = true,
+        unusedwrite = true,
+        -- useany = true,
       },
+      codelenses = {
+        gc_details = false,
+        generate = true,
+        regenerate_cgo = true,
+        run_govulncheck = true,
+        test = true,
+        tidy = true,
+        upgrade_dependency = true,
+        vendor = true,
+      },
+      completeUnimported = true,
+      gofumpt = true,
+      hints = { constantValues = true },
+      semanticTokens = true,
+      staticcheck = true,
+      usePlaceholders = true,
     },
   },
 }
