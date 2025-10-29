95313b2137ed5bf1f6b1fcfba209c595bf8425ec
Ephemeral <theephemeral.txt@gmail.com>
2025-10-29 13:52:59 +0600

    Revert "lsp(gopls): update gopls config"
    This reverts commit aad9f01b0f25b14826679672ca084d139fe6c008.

--- lsp/gopls.lua
+++ lsp/gopls.lua
@@ -1,29 +1,11 @@
 return {
   settings = {
     gopls = {
-      analyses = {
-        -- fieldalignment = true,
-        -- nilness = true,
-        unusedparams = true,
-        unusedwrite = true,
-        -- useany = true,
-      },
-      codelenses = {
-        gc_details = false,
-        generate = true,
-        regenerate_cgo = true,
-        run_govulncheck = true,
-        test = true,
-        tidy = true,
-        upgrade_dependency = true,
-        vendor = true,
-      },
       completeUnimported = true,
-      gofumpt = true,
-      hints = { constantValues = true },
-      semanticTokens = true,
-      staticcheck = true,
-      usePlaceholders = true,
+      usePlaceholders = false,
+      analyses = {
+        unusedparams = true,
+      },
     },
   },
 }
