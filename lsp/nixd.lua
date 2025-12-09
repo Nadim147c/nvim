02a0e414da978ff70d746fc8111aacaaf296c9f4
Ephemeral <theephemeral.txt@gmail.com>
2025-12-09 10:35:26 +0600

    nixd: load denix nixos and home options

--- lsp/nixd.lua
+++ lsp/nixd.lua
@@ -1,5 +1,15 @@
 local m = require "configs.lsp"
-return {
+
+-- Get current user and hostname
+local username = os.getenv "USER" or os.getenv "USERNAME"
+local hostname = os.getenv "HOSTNAME" or io.popen("hostname"):read "*l"
+
+-- Fallback values if not found
+username = username or "ephemeral"
+hostname = hostname or "chronoshift"
+
+-- Create nixd settings with dynamic values
+local nixd_settings = {
   capabilities = m.capabilities,
   cmd = { "nixd", "--inlay-hints=true", "--semantic-tokens=true" },
   settings = {
@@ -8,13 +18,43 @@ return {
         expr = "import <nixpkgs> { }",
       },
       formatting = {
-        command = { "alejandra" }, -- or nixfmt or nixpkgs-fmt
+        command = { "alejandra" },
       },
       options = {
-        home_manager = {
-          expr = '(builtins.getFlake "/home/ephemeral/git/dotfiles").homeConfigurations.ephemeral.options',
+        nixos = {
+          expr = string.format(
+            [[
+            let
+              nixosOptions = (builtins.getFlake (builtins.toString ./.)).nixosConfigurations."%s".options;
+              options = {
+                nixos.always = nixosOptions;
+                nixos.ifEanbled = nixosOptions;
+              };
+            in
+              options
+          ]],
+            hostname
+          ),
+        },
+        home = {
+          expr = string.format(
+            [[
+            let
+              homeOptions = (builtins.getFlake (builtits.toString ./.)).homeConfigurations."%s@%s".options;
+              options = {
+                home.always = homeOptions;
+                home.ifEanbled = homeOptions;
+              };
+            in
+              options
+          ]],
+            username,
+            hostname
+          ),
         },
       },
     },
   },
 }
+
+return nixd_settings
